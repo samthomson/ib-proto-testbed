9e7adba6b1615739fe17d45cbfd6a7bd672eb99a
sam <samt@samt.st>
2023-10-18 18:27:12 +0700

    linting

--- app/server/src/server.ts
+++ app/server/src/server.ts
@@ -1,20 +1,20 @@
 // Install the ws package first: npm install ws
-import WebSocket from 'ws';
+import WebSocket from 'ws'
 
 // Create a WebSocket server
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8080 })
 
 // Set up a connection event
 wss.on('connection', (ws: WebSocket) => {
-  console.log('New client connected');
+    console.log('New client connected')
 
-  // Set up message event for this connection
-  ws.on('message', (message: string) => {
-    console.log(`Received message: ${message}`);
-  });
+    // Set up message event for this connection
+    ws.on('message', (message: string) => {
+        console.log(`Received message: ${message}`)
+    })
 
-  // Send a welcome message to the client
-  ws.send('Welcome to the server!');
-});
+    // Send a welcome message to the client
+    ws.send('Welcome to the server!')
+})
 
-console.log('WebSocket server started on ws://localhost:8080');
+console.log('WebSocket server started on ws://localhost:8080')
