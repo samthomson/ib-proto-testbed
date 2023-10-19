e3a9451c40caad8261f65b5c2282c840c0d2a824
sam <samt@samt.st>
2023-10-19 14:38:01 +0700

    basic but messy ws flow

--- app/client/src/components/App.tsx
+++ app/client/src/components/App.tsx
@@ -1,18 +1,57 @@
-import React, { useEffect } from "react"
+import React from 'react'
+
+// todo: share this type
+interface Invoice {
+	amount: number
+	creationDate: string
+	expiryDate: string
+	status: 'paid' | 'unpaid'
+	lnAddress: string
+}
 
 const App = () => {
-	useEffect(() => {
+	const [invoices, setInvoices] = React.useState<Invoice[]>([])
+
+	const wsRef = React.useRef<WebSocket | null>(null);
+
+	React.useEffect(() => {
 		// Initialize WebSocket connection
-		const ws = new WebSocket("ws://localhost:3601")
+		const ws = new WebSocket('ws://localhost:3601')
+		wsRef.current = ws;
+
+		// if (!ws) {return}
 
 		// Connection opened
-		ws.addEventListener("open", (event) => {
-			ws.send("Hello Server!")
+		ws.addEventListener('open', () => {
+			ws.send(JSON.stringify({action: 'connection', value: "Hello Server!"}))
 		})
 
 		// Listen for messages from server
-		ws.addEventListener("message", (event) => {
-			console.log("Message from server:", event.data)
+		ws.addEventListener('message', (event) => {
+			console.log('Message from server:', event?.data)
+
+			const parsedData = JSON.parse(event.data)
+
+			switch (parsedData.action) {
+				case 'newInvoice':
+					setInvoices((prevInvoices) => [
+						...prevInvoices,
+						parsedData.invoice,
+					])
+					break
+				case 'allInvoices':
+					setInvoices(parsedData.invoices)
+					break
+				case 'invoicePaid':
+					setInvoices((prevInvoices) =>
+						prevInvoices.map((inv) =>
+							inv.lnAddress === parsedData.invoice.lnAddress
+								? parsedData.invoice
+								: inv,
+						),
+					)
+					break
+			}
 		})
 
 		// Cleanup
@@ -21,7 +60,29 @@ const App = () => {
 		}
 	}, [])
 
-	return <div>[web sockets?]</div>
+	const sendTip = () => {
+		const amount = Math.floor(Math.random() * 1000) + 1
+
+		wsRef.current?.send(JSON.stringify({action: 'createInvoice', amount }))
+	}
+
+	return (
+		<div>
+			<p>[todo: form for creating invoices]
+				<button onClick={sendTip}>send tip</button>
+			</p>
+			{(invoices ?? []).length > 0 && (
+				<div>
+					<h4>Past tips</h4>
+					<ul>
+						{invoices.map((invoice, key) => (
+							<li key={key}>{invoice.amount}</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default App
--- app/server/src/server.ts
+++ app/server/src/server.ts
@@ -5,17 +5,61 @@ import WebSocket from 'ws'
 const port = 8080
 const wss = new WebSocket.Server({ port })
 
+interface Invoice {
+    amount: number
+    creationDate: Date
+    expiryDate: Date
+    status: 'paid' | 'unpaid'
+    lnAddress: string
+}
+
+const recentInvoices: Invoice[] = []
+
 // Set up a connection event
 wss.on('connection', (ws: WebSocket) => {
-    console.log('New client connected')
+    console.log('New client connected?')
 
     // Set up message event for this connection
     ws.on('message', (message: string) => {
         console.log(`Received message: ${message}`)
-    })
 
-    // Send a welcome message to the client
-    ws.send('Welcome to the server!')
+        // Send a welcome message to the client
+        ws.send(JSON.stringify({ action: 'connection', connected: true }))
+
+        const parsedMessage = JSON.parse(message.toString())
+        switch (parsedMessage.action) {
+            case 'createInvoice':
+                // Simulate creating an invoice
+                const newInvoice: Invoice = {
+                    amount: parsedMessage.amount,
+                    creationDate: new Date(),
+                    expiryDate: new Date(Date.now() + 3600000), // 1 hour from now
+                    status: 'unpaid',
+                    lnAddress: 'ln-address-here',
+                }
+                recentInvoices.push(newInvoice)
+                // ws.send(JSON.stringify({ action: 'newInvoice', invoice: newInvoice }))
+                console.log('will now send out invoices', recentInvoices)
+                ws.send(JSON.stringify({ action: 'allInvoices', invoices: recentInvoices }))
+
+                // simulate it getting paid after 10s (in reality, this would come from watching the node)
+                setInterval(() => {
+                    if (recentInvoices.length > 0) {
+                        recentInvoices[recentInvoices.length - 1].status = 'paid'
+                        ws.send(
+                            JSON.stringify({
+                                action: 'invoicePaid',
+                                invoice: recentInvoices[recentInvoices.length - 1],
+                            }),
+                        )
+                    }
+                }, 10000)
+        }
+    })
 })
+
+// const emitInvoices = () => {
+//     wss.emit('newInvoice', { invoices: recentInvoices })
+// }
 
 console.log(`WebSocket server started on ws://localhost:${port}`)
